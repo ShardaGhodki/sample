07f4c805a505c88beae2b9aeb4c067d277f6de81
Sharda <ghodkisharda@gmail.com>
2024-06-12 12:16:11 +0530

    Your commit message

---
+++ test.py
@@ -0,0 +1,19 @@
+import logging
+import random
+
+print ("hello");
+
+
+def isEven():
+    $_current_number = random.randint(1,100)
+    print (current_number)
+    if current_number % 2 != 0:
+        return "odd"
+    else:
+        return "even"
+
+wasiteven = iEven()
+
+print(wasiteven)
+print(wasiteven)
+print(wasiteven)
